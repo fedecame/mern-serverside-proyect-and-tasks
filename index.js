9afa10a74fd953cd256a4a4cf5fc8fd0a96e3377
fedecame <fede.cameriere@gmail.com>
2020-03-14 21:34:15 -0300

    ajustes para deployment en heroku

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json({extended: true}));
 
 // puerto del app
-const PORT = process.env.PORT || 4000;
+const port = process.env.port || 4000;
 
 // Importar rutas
 app.use('/api/usuarios', require('./routes/usuarios'));
@@ -29,6 +29,6 @@ app.use('/api/tareas', require('./routes/tareas'));
 // });
 
 // arranca app
-app.listen(PORT, () => {
+app.listen(port, '0.0.0.0', () => {
     console.log(`El servidor esta funcionando en el puerto ${PORT}`);
 });
